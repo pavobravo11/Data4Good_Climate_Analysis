c1e1f15dccedda2d945fda9619106d1e251a8ee4
Manuel Schönlaub <manuel.schoenlaub@gmail.com>
2022-04-08 10:17:29 -0600

    Improve display

--- HistoricalGraph.py
+++ HistoricalGraph.py
@@ -176,10 +176,10 @@ def runner():
                 (1 / len(y_colors) * i),
                 (1 / len(y_colors) * (i + 1)) - 0.01
             ],
-            "title": f"{data_names[i]} ({units[i]})" if units[i] != "AQI" else "AQI",
+            "title": f"{data_names[i]} <br> ({units[i]})" if units[i] != "AQI" else "AQI",
             "linecolor": f"#{y_colors[i]}",
             "tickfont": {"color": f"#{y_colors[i]}"},
-            "titlefont": {"color": f"#{y_colors[i]}", "size": 8},
+            "titlefont": {"color": f"#{y_colors[i]}", "size": 12},
         }
         axis_params[f"yaxis{i + 1}" if i else "yaxis"] = {**common_params, **specific_params}
 
